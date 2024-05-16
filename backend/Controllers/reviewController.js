ff426f942a3c0d078598d22dbb3b9af9e30aefca
Irumi Rajamanthri <irumidisara@gmail.com>
2024-05-16 19:39:00 +0530

    Reviews

---
+++ backend/Controllers/reviewController.js
@@ -0,0 +1,38 @@
+import Review from "../models/ReviewSchema.js";
+import ServiceProvider from "../models/ServiceProviderSchema.js";
+
+//get all reviews
+export const getAllReviews = async (req, res) => {
+  try {
+    const reviews = await Review.find({});
+
+    res
+      .status(200)
+      .json({ success: true, message: "Successfull", data: reviews });
+  } catch (error) {
+    res.status(404).json({ success: false, message: "Not Found" });
+  }
+};
+
+//create review
+export const createReview = async (req, res) => {
+  if (!req.body.serviceprovider)
+    req.body.serviceprovider = req.params.serviceproviderId;
+  if (!req.body.user) req.body.user = req.userId;
+
+  const newReview = new Review(req.body);
+
+  try {
+    const savedReview = await newReview.save();
+
+    await ServiceProvider.findByIdAndUpdate(req.body.serviceprovider, {
+      $push: { reviews: savedReview._id },
+    });
+
+    res
+      .status(200)
+      .json({ success: true, message: "Review Submitted", data: savedReview });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
