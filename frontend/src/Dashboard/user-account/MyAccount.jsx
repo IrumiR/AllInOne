d692577433fb19cf4947191ca8f3c354bc871f3e
Irumi Rajamanthri <irumidisara@gmail.com>
2024-05-20 20:07:05 +0530

    Account UI of users

--- frontend/src/Dashboard/user-account/MyAccount.jsx
+++ frontend/src/Dashboard/user-account/MyAccount.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import React from "react";
+import userImg from "../../assets/images/patient-avatar.png";
+import { useContext, useState } from "react";
+import { authContext } from "../../context/AuthContext";
 
 const MyAccount = () => {
-  return (
-    <div>MyAccount</div>
-  )
-}
+  const { dispatch } = useContext(authContext);
+  const [tab, setTab] = useState("bookings");
 
-export default MyAccount
+  const handleLogout = () => {
+    dispatch({ type: "LOGOUT" });
+  };
+
+  return (
+    <div className="max-w-[1170px] px-5 mx-auto">
+      <div className="flex flex-col md:flex-row gap-10">
+        <div className="pb-[50px] px-[30px] rounded-md flex flex-col items-center md:items-start">
+          <div className="flex items-center justify-center">
+            <figure className="w-[100px] h-[100px] rounded-full border-2 border-solid border-primaryColor">
+              <img
+                src={userImg}
+                alt=""
+                className="w-full h-full rounded-full"
+              />
+            </figure>
+          </div>
+
+          <div className="text-center mt-4 md:text-left">
+            <h3 className="text-[18px] leading-[30px] text-headingColor font-bold">
+              Peter Parker
+            </h3>
+            <p className="text-textColor text-[15px] leading-6 font-medium">
+              example@gmail.com
+            </p>
+          </div>
+
+          <div className="mt-[20px] md:mt-[50px] w-full">
+            <button
+              onClick={handleLogout}
+              className="w-full bg-[#181A1E] p-3 text-[16px] leading-7 rounded-md text-white"
+            >
+              Logout
+            </button>
+            <button className="w-full bg-red-600 mt-4 p-3 text-[16px] leading-7 rounded-md text-white">
+              Delete Account
+            </button>
+          </div>
+        </div>
+
+        <div className="md:flex-1 md:px-[30px]">
+          <div className="flex justify-center md:justify-start">
+            <button
+              onClick={() => setTab("bookings")}
+              className={`${tab === "bookings" && "bg-primaryColor text-white font-normal"} p-2 mr-5 px-5 rounded-md text-headingColor font-semibold text-[16px]
+              leading-7 border border-solid border-primaryColor`}
+            >
+              My Bookings
+            </button>
+            <button
+              onClick={() => setTab("settings")}
+              className={`${tab === "settings" && "bg-primaryColor text-white font-normal"} py-2 px-5 rounded-md text-headingColor font-semibold text-[16px]
+              leading-7 border border-solid border-primaryColor`}
+            >
+              Profile Settings
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MyAccount;
