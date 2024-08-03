2aeb84fd792879fd4ec51a192b7fd5e88d97ac65
PasinduO <mailbox.pasindu@gmail.com>
2024-08-03 18:48:16 +0530

    response changed

--- backend/Controllers/authController.js
+++ backend/Controllers/authController.js
@@ -204,13 +204,14 @@ export const login = async (req, res) => {
     let user = await User.findOne({ email });
 
     if (!user) {
-      return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
+      return res.status(400).json({ type: 'error', message: 'Invalid Credentials' });
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
 
     if (!isMatch) {
-      return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
+      // return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
+      return res.status(400).json({ type: 'error', message: 'Invalid Credentials' });
     }
 
     const payload = {
